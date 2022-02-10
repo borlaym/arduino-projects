c30a0faafcad5ac7f4211fed3c313bb91a7f7cb3
Marton Borlay <borlaym@gmail.com>
2022-02-10 17:38:48 +0100

    write on lcd display!

--- sketch_feb08a/sketch_feb08a.ino
+++ sketch_feb08a/sketch_feb08a.ino
@@ -20,20 +20,35 @@ void setup() {
   digitalWrite(READ_WRITE, LOW);
 
   // Other setup
-  function_set(true, true);
+  function_set(false, false);
+  
+//  display_on_off(false, false, false);
+  
   display_on_off(true, true, true);
-  clear_display();
-  return_home();
   entry_mode_set(true, false);
+    clear_display();
+    return_home();  
+  write_character('#');
   write_character('h');
-  write_character(0b01001000);
-//  sendInstruction(0b00100111);
+  write_character('o');
+  write_character('b');
+  write_character('b');
+  write_character('y');
+  write_character('-');
+  write_character('e');
+  write_character('v');
+  write_character('e');
+  write_character('r');
+  write_character('y');
+  write_character('d');
+  write_character('a');
+  write_character('y');
+
 }
 
 void sendInstruction(char instruction) {
   digitalWrite(REGISTER_SELECT, LOW);
-  
-  Serial.println(instruction, BIN);
+  digitalWrite(READ_WRITE, LOW);
   for (int i = 7; i >= 0; i--) {
     char mask = 1 << i;
     char result = instruction & mask;
@@ -48,13 +63,24 @@ void sendInstruction(char instruction) {
       Serial.println("0");
     }
   }
-  digitalWrite(ENABLE, HIGH);
   digitalWrite(ENABLE, LOW);
+  delay(5);
+  digitalWrite(ENABLE, HIGH);
+  delay(5);
+  digitalWrite(ENABLE, LOW);
+  delay(100);
+//  read_data();
 }
 
 void write_character(char character) {
+  Serial.println("Sending character");
   digitalWrite(REGISTER_SELECT, HIGH);
+  digitalWrite(READ_WRITE, LOW);
   Serial.println(character);
+  for (int i = 7; i >= 0; i--) {
+    pinMode(data_pins[i], OUTPUT);
+    
+  }
   for (int i = 7; i >= 0; i--) {
     char mask = 1 << i;
     char result = character & mask;
@@ -69,12 +95,36 @@ void write_character(char character) {
       Serial.println("0");
     }
   }
-  digitalWrite(ENABLE, HIGH);
   digitalWrite(ENABLE, LOW);
+  delayMicroseconds(1);
+  digitalWrite(ENABLE, HIGH);
+  delayMicroseconds(1);
+  digitalWrite(ENABLE, LOW);
+  delay(100);
+//  read_data();
   
 }
 
+void read_data() {
+  digitalWrite(REGISTER_SELECT, HIGH);
+  digitalWrite(READ_WRITE, HIGH);
+  digitalWrite(ENABLE, LOW);
+  delayMicroseconds(1);
+  digitalWrite(ENABLE, HIGH);
+  delayMicroseconds(1);
+  digitalWrite(ENABLE, LOW);
+  Serial.println("READ");
+  
+  for (int i = 7; i >= 0; i--) {
+    pinMode(data_pins[i], INPUT);
+    Serial.print(digitalRead(data_pins[i]));
+  }
+  Serial.println("--");
+  delay(50);
+}
+
 void function_set(boolean line_number, boolean font_type) {
+  Serial.println("Function set");
   char instruction = 0b00110000;
   if (line_number) {
     instruction += 0b00001000;
@@ -86,14 +136,17 @@ void function_set(boolean line_number, boolean font_type) {
 }
 
 void clear_display() {
+  Serial.println("Clear display");
   sendInstruction(0b00000001);
 }
 
 void return_home() {
+  Serial.println("Return home");
   sendInstruction(0b0000010);
 }
 
 void entry_mode_set(boolean increment, boolean shift) {
+  Serial.println("Entry mode set");
   char instruction = 0b00000100;
   if (increment) {
     instruction += 0b00000010;
@@ -105,6 +158,7 @@ void entry_mode_set(boolean increment, boolean shift) {
 }
 
 void display_on_off(boolean display, boolean cursor, boolean blink) {
+  Serial.println("Display on/off");
   char instruction = 0b00001000;
   if (display) {
     instruction += 0b00000100;
@@ -120,5 +174,5 @@ void display_on_off(boolean display, boolean cursor, boolean blink) {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
+
 }
